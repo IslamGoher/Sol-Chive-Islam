0da2ff70bd5a836b8e4f232c08e677cc314a2caf
SamGoher <sam.m.goher@gmail.com>
2021-08-29 21:38:28 +0200

    Handle promise rejection using try catch

--- src/controller/solutions.ts
+++ src/controller/solutions.ts
@@ -12,38 +12,45 @@ export const getAllSolutions = async (
   next: NextFunction
 ) => {
 
-  // for development
-  req.user = {
-    id: '6123b11636fc8714c8c962a1'
-  };
-
-  // selected properties of every solution
-  let querySelect = {
-    'problem': 1,
-    'createdAt': 1,
-    'perfectSolution.isExist': 1
-  };
-
-  const user = await User.
-    findById(req.user.id).
-    select('solutions -_id').
-    populate({
-      path: 'solutions',
-      select: '_id problem createdAt perfectSolution.isExist',
-      model: Solution
-    });
-
-  // check if there's no user
-  if(!user) {
-    return;
-  }
-
-  // check if there's no solutions
-  if(user.solutions.length == 0) {
-    return next(new ErrorResponse(404, "this account has no solutions"));
-  }
+  try {
+    
+    // for development
+    req.user = {
+      id: '6123b11636fc8714c8c962a1'
+    };
   
-  res.status(200).json(user.solutions);
+    // selected properties of every solution
+    let querySelect = {
+      'problem': 1,
+      'createdAt': 1,
+      'perfectSolution.isExist': 1
+    };
+  
+    const user = await User.
+      findById(req.user.id).
+      select('solutions -_id').
+      populate({
+        path: 'solutions',
+        select: '_id problem createdAt perfectSolution.isExist',
+        model: Solution
+      });
+  
+    // check if there's no user
+    if(!user) {
+      return;
+    }
+  
+    // check if there's no solutions
+    if(user.solutions.length == 0) {
+      return next(new ErrorResponse(404, "this account has no solutions"));
+    }
+    
+    res.status(200).json(user.solutions);
+
+  } catch (error) {
+    next(error);
+  }
+
 };
 
 // route:   GET '/api/v1/solutions/:solutiontId'
@@ -54,35 +61,40 @@ export const getOneSolution = async (
   res: Response,
   next: NextFunction
 ) => {
+  try {
 
-  // for development
-  req.user = {
-    id: '6123b11636fc8714c8c962a1'
-  };
-  
-  const solutionId = req.params.solutionId;
+    // for development
+    req.user = {
+      id: '6123b11636fc8714c8c962a1'
+    };
+    
+    const solutionId = req.params.solutionId;
 
-  const currentSolution = await Solution.
-    findById(solutionId).
-    select('mySolution perfectSolution problem createdAt user');
-  
-  // check if solution found
-  if(!currentSolution) {
-    return next(new ErrorResponse(404, "there's no solution found with given id"));
+    const currentSolution = await Solution.
+      findById(solutionId).
+      select('mySolution perfectSolution problem createdAt user');
+    
+    // check if solution found
+    if(!currentSolution) {
+      return next(new ErrorResponse(404, "there's no solution found with given id"));
+    }
+
+    // check if solution belongs to user
+    if(req.user.id !== currentSolution.user.toString()) {
+      return next(new ErrorResponse(403, "forbidden: can't access to this content"));
+    }
+    // remove user id before sending
+    let finalSolution = {
+      _id: currentSolution._id,
+      createdAt: currentSolution.createdAt,
+      problem: currentSolution.problem,
+      mySolution: currentSolution.mySolution,
+      perfectSolution: currentSolution.perfectSolution
+    };
+
+    res.status(200).json(finalSolution);
+    
+  } catch (error) {
+    next(error);
   }
-
-  // check if solution belongs to user
-  if(req.user.id !== currentSolution.user.toString()) {
-    return next(new ErrorResponse(403, "forbidden: can't access to this content"));
-  }
-  // remove user id before sending
-  let finalSolution = {
-    _id: currentSolution._id,
-    createdAt: currentSolution.createdAt,
-    problem: currentSolution.problem,
-    mySolution: currentSolution.mySolution,
-    perfectSolution: currentSolution.perfectSolution
-  };
-
-  res.status(200).json(finalSolution);
 }
