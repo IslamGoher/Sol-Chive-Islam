186075eef7bf388276e1bcaa137475e5ba8bd3e2
SamGoher <sam.m.goher@gmail.com>
2021-08-30 21:28:45 +0200

    Add api 'get user data'

---
+++ src/controller/users.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../model/user";
+
+// route:   GET '/api/v1/user'
+// desc:    get user data
+// access:  private (only logged in user can access his own data)
+export const getUserData = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+
+    // for development
+    req.user = {
+      id: "6123b11636fc8714c8c962a1"
+    }
+    
+    // find user
+    const currentUser = await User.
+      findById(req.user.id)
+      .select('name picture -_id');
+
+  res.status(200).json(currentUser);
+
+  } catch (error) {
+    next(error);
+  }
+};
